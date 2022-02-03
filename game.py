89a67dd92c0b808ebdfa5654cdc1854c4cec526e
kvo1999 <kav50@georgetown.edu>
2022-02-03 10:30:44 -0500

    finished v1

--- game.py
+++ game.py
@@ -6,12 +6,16 @@
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
 
+print("----------")
+print("Welcome 'Player One' to my Rock-Paper-Scissors game!")
+print("----------")
 # ask for a user input 
 
-userchoice = input(" Please choose one: 'Rock', 'Paper', or 'Scissors'? ")
-print("User chose", userchoice)
+userchoice = input(" Please choose one: 'Rock', 'Paper', or 'Scissors':")
+print("User chose:", userchoice)
 
 # validate the user input
+userchoice = userchoice.lower()
 if (userchoice != "rock") or (userchoice != "paper") or (userchoice != "scissors"):
      print("Please enter a valid input.")
      quit()
@@ -24,7 +28,36 @@ options = ["Rock","Paper","Scissors"]
 computer_choice = choice(options)
 print("Computer chose:", computer_choice)
 
-# determine the winner
+print("----------")
 
+# determine the winner
+#Nikita's code that she posted in Slack helped me out!
+def determine_the_winner(userchoice, computer_choice):
+    #user chose paper
+    if userchoice == "paper" and computer_choice == "rock":
+        print("You are the winner! :)")
+    elif userchoice == "paper" and computer_choice == "scissors":
+        print("You lost! :(")
+    elif userchoice == "paper" and computer_choice == "paper":
+        print("It's a tie! :D")
+    
+    #user chooses scissors
+    if userchoice == "scissors" and computer_choice == "rock":
+        print("You lost! :(")
+    elif userchoice == "scissors" and computer_choice == "scissors":
+        print("It's a tie! :D")
+    elif userchoice == "scissors" and computer_choice == "paper":
+        print("You are the winner! :)")
+
+    #user chooses rock
+    if userchoice == "rock" and computer_choice == "rock":
+        print("It's a tie! :D")
+    elif userchoice == "rock" and computer_choice == "scissors":
+        print("You are the winner! :)")
+    elif userchoice == "rock" and computer_choice == "paper":
+        print("You lost! :(")
+
+print("----------")
 
 # display the final results 
+print("Thank you for playing the game! Please play again!")
